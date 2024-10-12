e3699cc4a98c631da46705edf3e3514cb3eb3378
islam <bgbos7077@gmail.com>
2024-10-12 16:32:19 +0300

    Login screen contains now just text field and password field

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -9,60 +9,137 @@ class LoginScreen extends StatelessWidget {
     FontWeight subTextWeight = FontWeight.w400;
     double containerHight = MediaQuery.of(context).size.height * 0.5;
     return Scaffold(
-      body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 35),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            SizedBox(
-              height: 116,
-            ),
-            Container(
-              width: double.infinity,
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                mainAxisSize: MainAxisSize.min,
-                children: [
-                  Text(
-                    'Login ',
-                    style: TextStyle(
-                      color: MyColor.blue,
-                      fontSize: 40,
-                      fontFamily: 'Poppins',
-                      fontWeight: FontWeight.w700,
+      body: ListView(children: [
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 35),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              SizedBox(
+                height: 116,
+              ),
+              Container(
+                width: double.infinity,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    Text(
+                      'Login',
+                      style: TextStyle(
+                        color: MyColor.blue,
+                        fontSize: 32,
+                        fontFamily: 'Poppins',
+                        fontWeight: FontWeight.w700,
+                      ),
                     ),
-                  ),
-                  Text(
-                    'Login now to track all your expenses and income at a place! ',
-                    style: TextStyle(
-                      color: Colors.black,
-                      fontSize: 22,
+                    SizedBox(
+                      height: 10,
+                    ),
+                    Text(
+                      'Login now to track all your daily water intake ',
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 16,
+                        fontFamily: 'Poppins',
+                        fontWeight: subTextWeight,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              SizedBox(
+                height: 40,
+              ),
+              Container(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      'Email',
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 17,
+                        fontFamily: 'Poppins',
+                        fontWeight: subTextWeight,
+                      ),
+                    ),
+                    SizedBox(
+                      height: 5,
+                    ),
+                    TextFormField(
+                      decoration: InputDecoration(
+                        hintText: 'Ex: abc@example.com',
+                        hintStyle: TextStyle(color: Colors.grey),
+                        prefixIcon: Icon(
+                          Icons.alternate_email,
+                          color: MyColor.blue,
+                        ),
+                        enabledBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(16),
+                          borderSide:
+                              BorderSide(color: MyColor.blue, width: 1.5),
+                        ),
+                        focusedBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(16),
+                          borderSide:
+                              BorderSide(color: MyColor.blue, width: 1.5),
+                        ),
+                      ),
+                    ),
+                    SizedBox(height: 28),
+                    Text(
+                      'Your Password',
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 17,
+                        fontFamily: 'Poppins',
+                        fontWeight: subTextWeight,
+                      ),
+                    ),SizedBox(
+                      height: 5,
+                    ),
+                    TextFormField(
+                      obscureText: true,
+                      keyboardType: TextInputType.visiblePassword,
+                      decoration: InputDecoration(
+                        hintText: '***********',
+                        hintStyle: TextStyle(color: Colors.grey),
+                        prefixIcon: Icon(
+                          Icons.lock,
+                          color: MyColor.blue,
+                        ),
+                        enabledBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(16),
+                          borderSide:
+                              BorderSide(color: MyColor.blue, width: 1.5),
+                        ),
+                        focusedBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(16),
+                          borderSide:
+                              BorderSide(color: MyColor.blue, width: 1.5),
+
+                        ),
+                      ),
+                    ),
+                    SizedBox(height: 7,),
+                    Text('Forgot Password?', style:TextStyle(
+                      decoration: TextDecoration.underline,
+                      decorationColor: MyColor.blue,
+                      decorationThickness: 1.5,
+                      color: MyColor.blue,
+                      fontSize: 12,
                       fontFamily: 'Poppins',
                       fontWeight: subTextWeight,
-                    ),
-                  ),
-                ],
+                    ),)
+                  ],
+                ),
               ),
-            ),
-            SizedBox(
-              height: 15,
-            ),
-            Container(
-              width: double.infinity,
-              child: Column(
-                children: [
-                  OutlinedButton(
-                      onPressed: () {},
-                      child: Row(
-                        children: [Text('Continue with Google')],
-                      ))
-                ],
-              ),
-            )
-          ],
+            ],
+          ),
         ),
-      ),
+      ]),
     );
   }
 }
